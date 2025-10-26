f39faab02e6db9af959c04e8504579207fdb67a3
Ryan Bernard <rbernard143@usf.edu>
2025-10-26 13:57:09 -0400

    Updated Counter and Page javascript file to include buttons and added the function to reset the counter.

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Counter from "../components/Counter";
+
+export default function HomePage() {
+    return (
+        <main style={{padding:"2rem", fontFamily: "sans-serif"}}>
+            <h1>Button Counter</h1>
+        <section style = {{marginTop:"2rem"}}>
+            <h2>Counter (default)</h2>
+            <Counter />
+        </section>
+        <section style = {{marginTop:"2rem"}}>
+        <h2>Counter (custom props)</h2>
+        <Counter initialCounter={5} initialStep={2}></Counter>
+        </section>
+        </main>
+    );
+}
---
+++ components/counter.js
@@ -0,0 +1,40 @@
+import {useState} from "react";
+export default function Counter({intialCount=0, intialStep=1}){
+    const [count,setCount] = useState(intialCount);
+    const [step, setStep] = useState(intialStep);
+
+    function increment(){
+        setCount(count+step);}
+    function decrement(){
+        if (count-step>=0){
+            setCount(count-step);}
+    }
+
+    function reset(){
+        setCount(0);
+    }
+
+    function handleStepChange(e){
+        const value = Number(e.target.value);
+        setStep(value>=1 ? value:1);
+    }
+
+    return(
+        <div className="counter">
+            <div className="count" aria-live="polite">
+                Counter:<strong>{count}</strong>
+            </div>
+
+            <div className="buttons">
+                <button onClick={increment}>+1</button>
+                <button onClick={decrement}>-1</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+
+            <label className="step-label">
+                Step:<input type="number" min="1" value={step} onChange={handleStepChange}>
+            </input>         
+            </label>
+        </div>
+    );
+}
